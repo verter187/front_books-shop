a32e46b08dbf51413ef35d78eb88d26bb4ab80d4
verter187 <verter187@inbox.ru>
2022-06-25 14:01:35 +0300

    front minor improvements 20020625_1

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -14,7 +14,9 @@ export default function Book({ title, price, descr, type, img_name }) {
         <h2>{title}</h2>
         <h3>{`$ ${price} USD`}</h3>
         <p>{descr}</p>
-        <Block title={type} color={"#1B3764"} />
+        <div className={s.books_block}>
+          <Block title={type} color={"#1B3764"} />
+        </div>
       </div>
     </div>
   );
--- src/components/Pages/AuthorsBooks/AuthorsBooks.jsx
+++ src/components/Pages/AuthorsBooks/AuthorsBooks.jsx
@@ -5,15 +5,17 @@ import Title from "../../UI/Title";
 
 export default function AuthorsBooks({ books }) {
   return (
-    <>
+    <div className={s.author_books}>
       <Title>The Author’s Book</Title>
-      <div className={s.author_books}>
+      <div className={s.author_books_info}>
         <div className={s.container}>
-          {books.map((book, i) => (
-            <Book key={i} {...book} />
-          ))}
+          <div className={s.author_books_items}>
+            {books.map((book, i) => (
+              <Book key={i} {...book} />
+            ))}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
